f4c3398be93579e4cc00dbedca277ab5d58920c8
Vladimir Revyagin <skorabeyf@gmail.com>
2024-06-28 15:29:42 +0300

    feat(useHover.js):код работает

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,8 +1,19 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { useState } from 'react'
 
 export default function useHover() {
-  const [hovered] = useState('1')
-  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
+  const ref = useRef(null)
+
+  const handleMouseOver = () => setHovered(true)
+  const handleMouseOut = () => setHovered(false)
+
+  useEffect(() => {
+    const refState = ref.current
+    if (refState !== null)
+      refState.addEventListener('mouseover', handleMouseOver)
+    refState.addEventListener('mouseout', handleMouseOut)
+  }, [ref])
+
   return { hovered, ref }
 }
